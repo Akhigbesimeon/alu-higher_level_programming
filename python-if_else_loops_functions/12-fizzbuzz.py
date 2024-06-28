6c1ceb1f9ab1c0a50677315db57f2e525726c424
Akhigbesimeon <s.akhigbe@alustudent.com>
2024-06-28 16:17:42 -0700

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 12-fizzbuzz.py
 
+
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
@@ -11,4 +12,3 @@ def fizzbuzz():
             print("Buzz ", end="")
         else:
             print("{} ".format(number), end="")
-
